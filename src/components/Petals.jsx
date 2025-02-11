3730f38c31ccb62722c6e49dfa8647fed51a9138
12Aleks <tanais2010@yahoo.com>
2025-02-11 10:52:27 +0100

    update code in the Petals component

--- src/components/Petals.jsx
+++ src/components/Petals.jsx
@@ -1,33 +1,47 @@
+"use client"
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 
+const createPetal = () => ({
+    id: Math.random(),
+    top: `${Math.random() * 100}%`,
+    left: `${Math.random() * 100}%`,
+    animationDuration: `${Math.random() * 5 + 5}s`,
+    animationDelay: `${Math.random() * 3}s`,
+    rotation: `${Math.random() * 360}deg`,
+});
+
 const Petals = () => {
+    const [petals, setPetals] = useState([]);
+
+    useEffect(() => {
+        const intervalShow = () => {
+            const newPetal = createPetal();
+            setPetals(currentPetals => [...currentPetals.slice(-8), newPetal]);
+        };
+        const interval = setInterval(intervalShow, 10000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className="fixed z-20 h-[360px] w-[380px] xxs:w-[400px] xxs:h-[390px] xs:w-[550px] xs:h-[500px] sm:w-[600px] sm:h-[550px] lg:w-[490px] lg:h-[430px] 2xl:w-[490px] 2xl:h-[390px] 3xl:w-[600px] 3xl:md:h-[550px] flex items-center justify-center mb-0 xxs:mb-0 xs:mb-0 sm:mb-14">
-            <div className={clsx( 'flex items-center justify-center relative top-20 left-0 w-72 h-80')}>
-                <div className="absolute top-10 left-10 w-0.5 h-0.5 3xl:w-1 3xl:h-1 bg-pink-50 opacity-0 rotate-45 animate-petals"
-                     style={{borderRadius: '50% 50% 50% 0', animationDuration: '8s, 4s', animationDelay: '0.5s, 0s'}}></div>
-
-                <div className="absolute top-10 left-20 w-0.5 h-0.5 3xl:w-1 3xl:h-1 bg-pink-100 opacity-0 rotate-45 animate-petals "
-                     style={{borderRadius: '50% 50% 50% 0', animationDuration: '10s, 5s', animationDelay: '1s, 0.5s'}}></div>
-
-                <div className="absolute top-10 left-32 w-0.5 h-0.5 3xl:w-1 3xl:h-1 bg-pink-200 opacity-0 animate-petals"
-                     style={{borderRadius: '50% 50% 50% 0', animationDuration: '7s, 3.5s', animationDelay: '1.5s, 1s'}}></div>
-
-                <div className="absolute top-10 left-40 w-0.5 h-0.5 3xl:w-1 3xl:h-1 bg-pink-100 opacity-0 rotate-45 animate-petals"
-                     style={{borderRadius: '50% 50% 50% 0', animationDuration: '10s, 6s', animationDelay: '2s, 1.5s'}}></div>
-
-                <div className="absolute top-10 left-52 w-0.5 h-0.5 3xl:w-1 3xl:h-1 bg-pink-300 opacity-0 rotate-90 animate-petals"
-                     style={{borderRadius: '50% 50% 50% 0', animationDuration: '6s, 3s', animationDelay: '2.5s, 2s'}}></div>
-
-                <div className="absolute top-10 left-60 w-0.5 h-0.5 3xl:w-1 3xl:h-1 bg-pink-100 opacity-0 rotate-12 animate-petals"
-                     style={{borderRadius: '50% 50% 50% 0', animationDuration: '8.5s, 4.5s', animationDelay: '3s, 2.5s'}}></div>
-
-                <div className="absolute top-10 left-56 w-0.5 h-0.5 3xl:w-1 3xl:h-1 bg-pink-100 opacity-0 rotate-3 animate-petals"
-                     style={{borderRadius: '50% 50% 50% 0', animationDuration: '9.5s, 5.5s', animationDelay: '3.5s, 3s'}}></div>
-
-                <div className="absolute top-10 left-24 w-0.5 h-0.5 3xl:w-1 3xl:h-1 bg-pink-50 opacity-0 rotate-90 animate-petals"
-                     style={{borderRadius: '50% 50% 50% 0', animationDuration: '7.5s, 4s', animationDelay: '4s, 3.5s'}}></div>
-            </div>
+        <div className="fixed z-20 h-[250px] w-[200px] 2xl:w-[200px] 2xl:h-[250px] 3xl:w-[250px] 3xl:md:h-[350px] flex items-center justify-center mb-0 xxs:mb-0 xs:mt-0 sm:mt-14">
+            {petals.map((petal) => (
+                <div
+                    key={petal.id}
+                    className={clsx(
+                        "absolute rounded-full bg-pink-200 opacity-0 animate-petals w-0.5 h-0.5 3xl:w-[3px] 3xl:h-[3px]"
+                    )}
+                    style={{
+                        top: petal.top,
+                        left: petal.left,
+                        animationDuration: petal.animationDuration,
+                        animationDelay: petal.animationDelay,
+                        transform: `rotate(${petal.rotation})`,
+                        borderRadius: '50% 50% 50% 0',
+                    }}
+                ></div>
+            ))}
         </div>
     );
 };
