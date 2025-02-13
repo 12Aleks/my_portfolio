2821e507864c36ebaf371d4f7df12376e06053f9
12Aleks <tanais2010@yahoo.com>
2025-02-13 13:12:44 +0100

    new data

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -1,4 +1,3 @@
-// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
@@ -8,20 +7,8 @@ class MyDocument extends Document {
                 <Head>
                     {/* Скрипт Google Tag Manager для клиентской стороны */}
                     <script
-                        dangerouslySetInnerHTML={{
-                            __html: `
-                (function(w,d,s,l,i){
-                  w[l]=w[l]||[];
-                  w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});
-                  var f=d.getElementsByTagName(s)[0],
-                      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                  j.async=true;
-                  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                  f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-59ZKV4TH');
-              `,
-                        }}
+                        async
+                        src={`https://www.googletagmanager.com/gtm.js?id=GTM-59ZKV4TH`}
                     />
                 </Head>
                 <body>
@@ -31,7 +18,7 @@ class MyDocument extends Document {
                         src="https://www.googletagmanager.com/ns.html?id=GTM-59ZKV4TH"
                         height="0"
                         width="0"
-                        style={{ display: "none", visibility: "hidden" }}
+                        style={{ display: 'none', visibility: 'hidden' }}
                     ></iframe>
                 </noscript>
 
